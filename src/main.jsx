a0966ff8c9ba620795bc3caf4c34c9f3c901a552
vitorlimaid1-code <vitorlimaid1@gmail.com>
2025-12-18 10:39:17 -0300

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
+import './index.css' // <-- Isso aqui importa o código do Passo B
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
